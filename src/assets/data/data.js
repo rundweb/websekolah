727c8d771cb070a8bf50fa7520c7514f329eac09
faldo <pn4066283@gmail.com>
2024-06-11 16:41:11 +0700

    navbar & home

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -40,3 +40,25 @@ export const menu = [
     path: "/faq",
   },
 ];
+
+import gedung1 from "../images/homepages/gedung (1).jpg";
+import gedung2 from "../images/homepages/gedung (2).jpg";
+import gedung3 from "../images/homepages/gedung (3).jpg";
+
+export const dataImage = [
+  {
+    id: 1,
+    image: gedung1,
+    text: "Kami menyambut Anda dengan hangat di halaman utama sekolah kami, SMAN 1 Indonesia. Di sini, Anda akan menemukan informasi yang lengkap tentang pendidikan berkualitas yang kami tawarkan.",
+  },
+  {
+    id: 1,
+    image: gedung2,
+    text: "Kami menyambut Anda dengan hangat di halaman utama sekolah kami, SMAN 1 Indonesia. Di sini, Anda akan menemukan informasi yang lengkap tentang pendidikan berkualitas yang kami tawarkan.",
+  },
+  {
+    id: 1,
+    image: gedung3,
+    text: "Kami menyambut Anda dengan hangat di halaman utama sekolah kami, SMAN 1 Indonesia. Di sini, Anda akan menemukan informasi yang lengkap tentang pendidikan berkualitas yang kami tawarkan.",
+  },
+];
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,10 +1,38 @@
+import "./css/homepages.css";
+import { dataImage } from "../assets/data/data";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { NavLink } from "react-router-dom";
 
 const HomePages = () => {
   return (
-    <div>
-      <h1>Home pages</h1>
-    </div>
-  )
-}
+    <section className="beranda">
+      <div className="beranda-content">
+        <Swiper className="mySwiper">
+          {dataImage.map((datas) => {
+            return (
+              <SwiperSlide key={datas.id} className="beranda-img-content">
+                <div className="beranda-crop-img">
+                  <img
+                    src={datas.image}
+                    alt="foto bangunan sekolah"
+                    className="beranda-img"
+                  />
 
-export default HomePages
+                  <div className="text-beranda">
+                    <h1>Selamat Datang Di SMAN 1 Indonesia</h1>
+                    <h2>{datas.text}</h2>
+                    <NavLink to="/berita">Hubungi Kami</NavLink>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default HomePages;
